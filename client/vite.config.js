c29fff7262e9326602b532fe179682e281e86940
Alisson Rubens <alissonrubens.s@gmail.com>
2026-01-16 10:27:15 -0300

    feat: update Vite configuration; use path.resolve for alias definitions to improve compatibility

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from "vite";
+import { fileURLToPath } from 'url'
 import react from "@vitejs/plugin-react";
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src",
-      "@services": "/services",
-      "@components": "/src/components",
-      "@pages": "/src/components/Pages",
-      "@content": "/src/components/Content",
-      "@utils": "/src/utils",
+      '@': [path.resolve(__dirname, '/src')],
+      '@services': [path.resolve(__dirname, '/services')],
+      '@components': [path.resolve(__dirname, '/src/components')],
+      '@pages': [path.resolve(__dirname, '/src/components/Pages')],
+      '@content': [path.resolve(__dirname, '/src/components/Content')],
+      '@utils': [path.resolve(__dirname, '/src/utils')],
     },
   },
 });
